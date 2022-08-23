4bbe1ab05a79da2f8507ab282049703f763cd968
vladponcea <vladponcea@icloud.com>
2022-08-23 14:56:51 +0300

    added extensions

---
+++ instagram_clone/instagram_clone/Resources/Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  Extensions.swift
+//  instagram_clone
+//
+//  Created by Vladut Mihai Poncea on 23.08.2022.
+//
+
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.origin.x + self.frame.size.width
+    }
+}
