770ac5bd5356615430e32cc08acb1aead79056dd
vladponcea <vladponcea@icloud.com>
2022-08-23 11:48:00 +0300

    initial commit

---
+++ instagram_clone/instagram_clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram_clone
+//
+//  Created by Vladut Mihai Poncea on 23.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
