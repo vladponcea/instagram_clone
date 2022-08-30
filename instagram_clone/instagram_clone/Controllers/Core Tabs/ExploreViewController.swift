909e874e7a9f170ae012ab05e771d8a517773646
vladponcea <vladponcea@icloud.com>
2022-08-30 10:52:34 +0300

    explore controller

--- instagram_clone/instagram_clone/Controllers/Core Tabs/ExploreViewController.swift
+++ instagram_clone/instagram_clone/Controllers/Core Tabs/ExploreViewController.swift
@@ -9,8 +9,41 @@ import UIKit
 
 class ExploreViewController: UIViewController {
 
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.backgroundColor = .secondarySystemBackground
+        return searchBar
+    }()
+    
+    private var collectionView: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.view.backgroundColor = .systemBackground
+        self.navigationController?.navigationBar.topItem?.titleView = searchBar
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        self.collectionView?.delegate = self
+        self.collectionView?.dataSource = self
+        guard let collectionView = collectionView else {
+            return
+        }
+        
+        self.view.addSubview(collectionView)
+    }
+}
+
+extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
     }
 }
