4bc10381f11d306e6f03e3c77a7c30f141a3cd33
vladponcea <vladponcea@icloud.com>
2022-08-30 15:15:13 +0300

    edit profile controller

--- instagram_clone/instagram_clone/Controllers/Other/General/EditProfileViewController.swift
+++ instagram_clone/instagram_clone/Controllers/Other/General/EditProfileViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class EditProfileViewController: UIViewController {
+struct EditProfileFormModel {
+    let label: String
+    let placeholder: String
+    var value: String?
+}
+
+final class EditProfileViewController: UIViewController, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(FormTableViewCell.self, forCellReuseIdentifier: FormTableViewCell.identifier)
+        return tableView
+    }()
+    
+    private var models = [[EditProfileFormModel]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .systemBackground
+        configureModels()
         
+        self.view.backgroundColor = .systemBackground
+        tableView.tableHeaderView = createTableHeaderView()
+        self.tableView.dataSource = self
+        self.view.addSubview(tableView)
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(
             title: "Save",
             style: .done,
@@ -28,12 +46,100 @@ class EditProfileViewController: UIViewController {
         )
     }
     
-    @objc private func didTapSave() {
+    private func configureModels() {
+        // name, username, website, bio
+        let section1Labels = ["Name", "Username", "Bio"]
+        var section1 = [EditProfileFormModel]()
+        for label in section1Labels {
+            let model = EditProfileFormModel(label: label, placeholder: "Enter \(label)...", value: nil)
+            section1.append(model)
+        }
+        models.append(section1)
+        
+        // email, phone, gender
+        let section2Labels = ["Email", "Phone", "Gender"]
+        var section2 = [EditProfileFormModel]()
+        for label in section2Labels {
+            let model = EditProfileFormModel(label: label, placeholder: "Enter \(label)...", value: nil)
+            section2.append(model)
+        }
+        models.append(section2)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.tableView.frame = self.view.bounds
+    }
+    
+    // MARK: - TableView
+    
+    private func createTableHeaderView() -> UIView {
+        let header = UIView(frame: CGRect(
+            x: 0,
+            y: 0,
+            width: self.view.width,
+            height: self.view.height/4).integral
+        )
+        
+        let size = header.height/1.5
+        
+        let profilePhotoButton = UIButton(
+            frame: CGRect(
+                x: (self.view.width-size)/2,
+                y: (header.height-size)/2,
+                width: size,
+                height: size
+            )
+        )
+        
+        header.addSubview(profilePhotoButton)
+        profilePhotoButton.layer.masksToBounds = true
+        profilePhotoButton.layer.cornerRadius = size/2.0
+        profilePhotoButton.tintColor = .label
+        profilePhotoButton.addTarget(self, action: #selector(didTapProfilePhotoButton), for: .touchUpInside)
+        
+        profilePhotoButton.setBackgroundImage(UIImage(systemName: "person.circle"), for: .normal)
+        profilePhotoButton.layer.borderWidth = 1
+        profilePhotoButton.layer.borderColor = UIColor.secondarySystemBackground.cgColor
+        
+        return header
+    }
+    
+    @objc private func didTapProfilePhotoButton() {
         
     }
     
-    @objc private func didTapCancel() {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.section][indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: FormTableViewCell.identifier, for: indexPath) as! FormTableViewCell
+        cell.configure(with: model)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard section == 1 else {
+            return nil
+        }
         
+        return "Private Information"
+    }
+    
+    // MARK: - Action
+    
+    @objc private func didTapSave() {
+        // Save info to database
+    }
+    
+    @objc private func didTapCancel() {
+        dismiss(animated: true, completion: nil)
     }
     
     @objc private func didTapChangeProfilePicture() {
