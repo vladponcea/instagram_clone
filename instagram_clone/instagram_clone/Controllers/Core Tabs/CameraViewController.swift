1ca52d5ff0b2d9b937156c464704fbc803ada178
vladponcea <vladponcea@icloud.com>
2022-08-30 10:47:52 +0300

    camera controller

--- instagram_clone/instagram_clone/Controllers/Core Tabs/CameraViewController.swift
+++ instagram_clone/instagram_clone/Controllers/Core Tabs/CameraViewController.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    private func didTapTakePicture() {
+        
     }
 }
