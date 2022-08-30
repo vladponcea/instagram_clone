6850257aa9e1550d9b5df50b9c76dbdd12d2ce5b
vladponcea <vladponcea@icloud.com>
2022-08-30 10:43:04 +0300

    log out

--- instagram_clone/instagram_clone/Controllers/Core Tabs/ProfileViewController.swift
+++ instagram_clone/instagram_clone/Controllers/Core Tabs/ProfileViewController.swift
@@ -7,10 +7,58 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+/// Profile view controller
+final class ProfileViewController: UIViewController {
 
+    private var collectionView: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemBackground
+        configureNavigationBar()
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: self.view.width/3, height: self.view.width/3)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        self.collectionView?.delegate = self
+        self.collectionView?.dataSource = self
+        guard let collectionView = collectionView else {
+            return
+        }
+        self.view.addSubview(collectionView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.collectionView?.frame = self.view.bounds
+    }
+    
+    private func configureNavigationBar() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettingsButton))
+    }
+    
+    @objc private func didTapSettingsButton() {
+        let vc = SettingsViewController()
+        vc.title = "Settings"
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
 
+
+extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.collectionView?.deselectItem(at: indexPath, animated: true)
     }
 }
--- instagram_clone/instagram_clone/Controllers/Other/General/SettingsViewController.swift
+++ instagram_clone/instagram_clone/Controllers/Other/General/SettingsViewController.swift
@@ -7,23 +7,94 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+struct SettingsCellModel {
+    let title: String
+    let handler: (() -> Void)
+}
 
+
+/// View Controller to show user settings
+final class SettingsViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private var data = [[SettingsCellModel]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configureModels()
+        
+        self.view.backgroundColor = .systemBackground
+        self.view.addSubview(tableView)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
-    */
+    
+    private func configureModels() {
+        let section = [
+            SettingsCellModel(title: "Log Out") { [weak self] in
+                self?.didTapLogOut()
+            }
+        ]
+        self.data.append(section)
+    }
+    
+    private func didTapLogOut() {
+        let actionSheet = UIAlertController(title: "Log Out", message: "Are you sure you want to log out?", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler: { _ in
+            AuthManager.shared.logOut { success in
+                DispatchQueue.main.async {
+                    if success {
+                        // present log in
+                        let loginVC = LoginViewController()
+                        loginVC.modalPresentationStyle = .fullScreen
+                        self.present(loginVC, animated: true) {
+                            self.navigationController?.popToRootViewController(animated: false)
+                            self.tabBarController?.selectedIndex = 0
+                        }
+                    } else {
+                        // error occured
+                        fatalError("Could not log out user")
+                    }
+                }
+            }
+        }))
+        
+        actionSheet.popoverPresentationController?.sourceView = self.tableView
+        actionSheet.popoverPresentationController?.sourceRect = self.tableView.bounds
+        present(actionSheet, animated: true)
+    }
+}
 
+
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.data.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = self.data[indexPath.section][indexPath.row].title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
+        data[indexPath.section][indexPath.row].handler()
+    }
 }
