4a9c4382bb442dd75408f627abe3638b31be6121
vladponcea <vladponcea@icloud.com>
2022-08-30 10:46:57 +0300

    notifications controller

--- instagram_clone/instagram_clone/Controllers/Core Tabs/NotificationsViewController.swift
+++ instagram_clone/instagram_clone/Controllers/Core Tabs/NotificationsViewController.swift
@@ -7,10 +7,35 @@
 
 import UIKit
 
-class NotificationsViewController: UIViewController {
+class NotificationsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title = "Notifications"
+        self.view.backgroundColor = .systemBackground
+        self.view.addSubview(tableView)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.tableView.frame = self.view.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
 }
